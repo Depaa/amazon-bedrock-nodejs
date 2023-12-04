83161eed41ef3872496ff29a12b726514a5e578d
Matteo Depascale <m.depascale@reply.it>
2023-12-04 14:25:36 +0100

    feat: add llama2 examples

---
+++ javascript/invoke-model-meta-llama2.js
@@ -0,0 +1,50 @@
+const { invokeModel } = require('./utils/client-bedrock-runtime');
+
+const MODEL_ID = process.env.MODEL_ID || 'meta.llama2-70b-chat-v1';
+/*
+* Meta Llama 2 models id:
+* 'meta.llama2-70b-chat-v1',
+* 'meta.llama2-13b-chat-v1',
+* @see https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids-arns.html
+*/
+
+const PROMPT = process.env.PROMPT || 'Hi, who are you?';
+
+exports.handler = async (event) => {
+  const params = {
+    modelId: MODEL_ID,
+    contentType: "application/json",
+    accept: "application/json",
+    body: JSON.stringify({
+      prompt: PROMPT,
+      temperature: 0.5,
+      top_p: 0.9,
+      max_gen_len: 512
+    }),
+  };
+
+  try {
+    const res = await invokeModel(params);
+    const jsonString = new TextDecoder().decode(res.body);
+    const modelRes = JSON.parse(jsonString);
+
+    const bodyRes = {
+      prompt: PROMPT,
+      completion: modelRes.generation,
+    }
+    console.debug(bodyRes);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(bodyRes),
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: err.message,
+      }),
+    };
+  }
+};
