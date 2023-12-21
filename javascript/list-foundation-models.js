f557691e1776751b57d22b6fc6c4e2ffb99b60d4
Matteo Depascale <m.depascale@reply.it>
2023-12-21 16:07:03 +0100

    feat: list foundation model update

--- javascript/list-foundation-models.js
+++ javascript/list-foundation-models.js
@@ -1,14 +1,7 @@
 const { listFoundationModels } = require("./utils/client-bedrock");
 
 exports.handler = async (event) => {
-  /*
-  * byInferenceType is "required" because currently a bug in the SDK prevents the request
-  * from being sent. The indicted parameters is byProvider.
-  * see bug report: https://github.com/aws/aws-sdk-js/issues/4519
-  */
-  const params = {
-    byInferenceType: "ON_DEMAND", // or "PROVISIONED"
-  }
+  const params = {}
   try {
     const res = await listFoundationModels(params);
     return {
--- typescript/list-foundation-models.ts
+++ typescript/list-foundation-models.ts
@@ -2,14 +2,7 @@ import { APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 import { ListFoundationModelsCommandInput, listFoundationModels } from "./utils/client-bedrock";
 
 export const handler: APIGatewayProxyHandler = async (_event): Promise<APIGatewayProxyResult> => {
-  /*
-  * byInferenceType is "required" because currently a bug in the SDK prevents the request
-  * from being sent. The indicted parameters is byProvider.
-  * see bug report: https://github.com/aws/aws-sdk-js/issues/4519
-  */
-  const params: ListFoundationModelsCommandInput = {
-    byInferenceType: "ON_DEMAND", // or "PROVISIONED"
-  }
+  const params: ListFoundationModelsCommandInput = {}
   try {
     const res = await listFoundationModels(params);
     return {
