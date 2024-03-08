11172d335b176760b78ced5d72ad741392bedc88
= <=>
2024-03-08 09:49:00 +0100

    feat: add mistral ai models

---
+++ javascript/invoke-model-mistral-ai.js
@@ -0,0 +1,52 @@
+const { invokeModel } = require('./utils/client-bedrock-runtime');
+
+const MODEL_ID = process.env.MODEL_ID || 'mistral.mixtral-8x7b-instruct-v0:1';
+/*
+* Cohere models id:
+* "mistral.mistral-7b-instruct-v0:2"
+* "mistral.mixtral-8x7b-instruct-v0:1"
+* @see https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids-arns.html
+*/
+
+const PROMPT = process.env.PROMPT || 'Hi, who are you?';
+
+exports.handler = async (event) => {
+  const params = {
+    modelId: MODEL_ID,
+    contentType: "application/json",
+    accept: "application/json",
+    body: JSON.stringify({
+      prompt: PROMPT,
+      max_tokens: 400,
+      temperature: 0.75,
+      top_p: 0.01,
+      // top_k: 0,
+      // stop: [],
+    }),
+  };
+
+  try {
+    const res = await invokeModel(params);
+    const jsonString = new TextDecoder().decode(res.body);
+    const modelRes = JSON.parse(jsonString);
+
+    const bodyRes = {
+      prompt: PROMPT,
+      completion: modelRes.outputs[0].text,
+    }
+    console.debug(bodyRes);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(bodyRes),
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: err.message,
+      }),
+    };
+  }
+};
